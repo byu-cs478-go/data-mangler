73eb9111a31969bdf1baae06917bde49b1ca5d9a
Parker Michaelson <parker.michaelson@gmail.com>
2015-03-31 11:40:40 -0600

    Added decent parser.

--- src/main.py
+++ src/main.py
@@ -1,22 +1,33 @@
 import sys
 import os
 import subprocess
+import copy
 
-# def tmpdir_init:
-#     if not os.path.exists('tmp'):
-#         os.makedirs('tmp')
 
-# def sgfstr_sgfarray_convert (sgfstr):
-#     for node in string.split(sgfstr, ';'):
-#         if node.split()
+
+# This header goes at the top of the generated .arff file. It includes
+# the attribute specifiers and tags up to '@data'.
+ARFFHEADER = ("")
+
+# This variable controls the number of board samples taken from a
+# given game.
+SAMPLERATE = 1
+
+
+
+def sgf_node_parse(nodestr):
+    pass
 
 def sgfarray_metadata_read():
+    pass
 
 def sgf_tree_build():
     pass
 
-def sgfarray_step(old, sgfstr):
-    
+def sgf_step_read(old, sgfstr):
+    pass
+
+def sgf_done_read(sgfstr)
 
 def sgf_process(sgfstr):
     # Get the states in the game that will be analyzed.
@@ -24,29 +35,129 @@ def sgf_process(sgfstr):
 
     # Run the analysis functions on the states.
     for state in states:
-        
+        pass
 
 
 
-def sgffile_process(infile, outfile):
-    # 
-    pass
+def sgfboard_empty_gen():
+    return [[0 for j in range (19)] for i in range(19)]
+
+
+
+# def sgfstr_sgfboard_step(instr, oldboard):
+#     newboard = copy.deepcopy(oldboard)
+
+#     tmpstr = instr.lstrip()
+
+#     # TODO Use a dict here? Assumes that lettering is
+#     # lowercase. Assumes no spaces inside property. Is orientation
+#     # going to be an issue?
+#     if   tmpstr[0] == 'W':
+#         newboard[ord(tmpstr[2]) - 96][ord(tmpstr[3]) - 96] = 1
+#     elif tmpstr[1] == 'B':
+#         newboard[ord(tmpstr[2]) - 96][ord(tmpstr[3]) - 96] = -1
+#     elif:
+#         return None
+#     else:
+#         # TODO Add additional diagnostic info. Global (cannot be set
+#         # from?) variables?
+#         sys.exit("Error: Unrecognized property.")
+
+
+
+def sgfstr_states_gen(instr):
+    boards = [sgfboard_empty_gen()]
+
+    prop = [None, None, None]
+    
+    for x in instr:
+        if   x == ' ' or x == '\t' or x == '\n':
+            pass
+        elif x == ';':
+            pass
+        elif x == '[':
+            pass
+        elif x == ']':
+            newboard = copy.deepcopy(boards[-1])
+            newboard[prop[1]][prop[2]] = prop[0]
+            boards.append(newboard)
+            prop = [None, None, None]    
+        elif x == '(':
+            pass
+        elif x == ')':
+            # if prop != [None, None, None]:
+            #     sys.exit("Error: Premature property end.")
+            # else:
+            break
+        elif x == 'W':
+            if prop[0] == None:
+                prop[0] = 1
+        elif x == 'B':
+            if prop[0] == None:
+                prop[0] = -1
+        # TODO Does not handle capital grid coordinates.
+        elif x in (a,t):
+            if prop[0] != None:
+                if prop[1] == None:
+                    prop[1] = ord(x) - 96
+                elif prop[2] == None:
+                    prop[2] = ord(x) - 96
+                else:
+                    sys.exit("Error: Too many coordinates in property.")
+        else:
+            sys.exit("Error: Parser broken.")
+
+    return boards
+
+
+
+def sgfstr_sample(instr):
+    # TODO Consume metadata.
+
+    states = [sgfboard_empty_gen()]
+
+    while states[-1] != None:
+        states.append(sgf_step_read(instr, states[-1]))
+
+    inc = floor((len(states)-1)/(SAMPLERATE + 1))
+    return [states[x] for x in range(inc,(len(states)-1),inc)]
+
+
+
+def sgfstr_process(instr):
+    data = []
+
+    boards = sgfstr_sample(instr)
+    for board in boards:
+        groups = getGroups(board)
+        boarddata = [[x] for x in getSizes(board, groups)]
+        data.extend(boarddata)
+
+    return data
+
+
+
+def arfffile_write(outfile, data):
+    # Prepend header.
+    outfile.write(ARFFHEADER)
+
+    # Dump the contents of data to the .arff file.
+    for x in data:
+        for y in x:
+            outfile.write("\t" + y + ",")
+        outfile.write("\n")
 
 
 
 def main():
-    if not os.path.exists(sys.argv[2]):
-        os.makedirs(sys.argv[2])
-    else:
-        sys.exit("Error: Specified tmp directory already exits.")
-
     cmd = 'find ' + sys.argv[1] + ' -name \'*.sgf\''
     sp = subprocess.Popen(cmd, universal_newlines=True, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
+    arffdata = []
     for inpath in sp.communicate()[0].split('\n'):
         if inpath != '':
-            outpath = sys.argv[2] + inpath[len(sys.argv[1]):] + ".mangle"
-            if not os.path.exists(os.path.dirname(outpath)):
-                os.makedirs(os.path.dirname(outpath))
-            with open(inpath, 'r') as infile, open(outpath, 'w') as outfile:
-                sgffile_process(infile, outfile)
+            with open(inpath, 'r') as infile:
+                arffdata.extend(sgfstr_process(infile.read()))
+
+    with open(sys.args[2], 'w') as arfffile:
+        arfffile_write(arfffile, arffdata)
