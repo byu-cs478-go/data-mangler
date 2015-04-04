21ac35b2e05bdc84972b14646525f4cf95327755
Christopher Almquist <almquist@byu.net>
2015-04-04 08:55:37 -0600

    Added sgf for testing and modified test to run tests.

--- src/test.py
+++ src/test.py
@@ -5,3 +5,10 @@ def test(inpath):
     with open(inpath, 'r') as infile:
         for x in main.sgfstr_sample(infile.read()):
             parserFunctions.showBoard(x)
+
+#test("BloodVomitingGame.sgf")
+#test("DosakusMasterpiece.sgf")
+#test("SeigenvsShusai.sgf")
+#test("ShusakuvsInseki.sgf")
+#test("ShuwavsInseki1.sgf")
+#test("ShuwavsInseki3.sgf")
